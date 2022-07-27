bdd6690bbaf8d5e877eb39991bcf0abcd41325d8
sir-hector <krauskarol7@gmail.com>
2022-07-27 22:59:01 +0200

    add maillist component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,20 +5,22 @@ import './home.css'
 import Featured from '../../components/featured/Featured'
 import PropertyList from '../../components/propertyList/PropertyList'
 import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
+import MailList from '../../components/mailList/MailList'
 
 const Home = () => {
   return (
     <div>
-      <Navbar/>
-      <Header/>
+      <Navbar />
+      <Header />
       <div className="homeContainer">
-        <Featured/>
+        <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
-        <PropertyList/>
+        <PropertyList />
         <h1 className="homeTitle">Homes guest love</h1>
-        <FeaturedProperties/>
+        <FeaturedProperties />
+        <MailList/>
       </div>
-      </div>
+    </div>
   )
 }
 
