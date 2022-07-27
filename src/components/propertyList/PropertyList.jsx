029d3bcb77851f107c62fda27b543f3954190635
sir-hector <krauskarol7@gmail.com>
2022-07-27 20:26:00 +0200

    add property List

---
+++ src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './propertyList.css'
+
+const PropertyList = () => {
+    return (
+        <div className='pList'>
+            <div className="pListItem">
+                <img src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=" alt="" className="pListImg" />
+                <div className="pListTitles">
+                    <h1>Hotels</h1>
+                    <h2>233 hotels</h2>
+                </div>
+            </div>
+            <div className="pListItem">
+                <img src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=" alt="" className="pListImg" />
+                <div className="pListTitles">
+                    <h1>Apartaments</h1>
+                    <h2>233 apartaments</h2>
+                </div>
+            </div>
+            <div className="pListItem">
+                <img src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=" alt="" className="pListImg" />
+                <div className="pListTitles">
+                    <h1>Resorts</h1>
+                    <h2>233 resorts</h2>
+                </div>
+            </div>
+            <div className="pListItem">
+                <img src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=" alt="" className="pListImg" />
+                <div className="pListTitles">
+                    <h1>Villas</h1>
+                    <h2>233 villas</h2>
+                </div>
+            </div>
+            <div className="pListItem">
+                <img src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=" alt="" className="pListImg" />
+                <div className="pListTitles">
+                    <h1>Cabins</h1>
+                    <h2>233 cabins</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PropertyList
