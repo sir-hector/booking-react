668e636b92e1d3cbbfe6718a15c6c599ae7c14fa
sir-hector <krauskarol7@gmail.com>
2022-07-28 22:36:30 +0200

    create single hotel component

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,8 +1,81 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar';
+import Header from '../../components/header/Header';
+import MailList from '../../components/mailList/MailList';
+import Footer from '../../components/footer/Footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import './hotel.css'
 
 const Hotel = () => {
+  const photos = [
+    {
+      src: "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
+    },
+    {
+      src: "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
+    },
+    {
+      src: "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
+    },
+    {
+      src: "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
+    },
+    {
+      src: "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
+    },
+    {
+      src: "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
+    },
+    {
+      src: "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
+    },
+    {
+      src: "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
+    },
+  ]
   return (
-    <div>Hotel</div>
+    <div>
+      <Navbar />
+      <Header type='list' />
+      <div className="hotelContainer">
+        <div className="hotelWrapper">
+          <button className="bookNow">Reserve or Book Now!</button>
+          <h1 className="hotelTitle">Grand Hotel</h1>
+          <div className="hotelAddress">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <span>Elton St 125 New York</span>
+          </div>
+          <span className="hotelDistance">Excellent location from center</span>
+          <span className="hotelPriceHighlight">Book a stay over $144 at this property and get a free airport taxi</span>
+          <div className="hotelImages">
+            {photos.map(photo => (
+              <div className="hotelImgWrapper">
+                <img src={photo.src} className="hotelImg" />
+              </div>
+            ))}
+          </div>
+          <div className="hotelDetails">
+            <div className="hotelDetailsTexts">
+              <h1 className="hotelTitle">Stay in the heart of Krakow</h1>
+              <p className="hotelDesc">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam earum ut quis aperiam numquam. Suscipit architecto alias corporis temporibus exercitationem provident rerum atque placeat a esse, ullam magnam expedita optio?
+              </p>
+            </div>
+            <div className="hotelDetailsPrice">
+              <h1>Perfect for a 9-night stay!</h1>
+              <span>Located in the real heart of Krakow</span>
+              <h2>
+                <b>$945</b> (9Nights)
+              </h2>
+              <button>Reserve or book Now!</button>
+            </div>
+          </div>
+        </div>
+        <MailList />
+        <Footer />
+      </div>
+    </div>
   )
 }
 
