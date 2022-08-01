be32fa54122062cfde47cb23d0009b40a07df080
sir-hector <krauskarol7@gmail.com>
2022-08-01 18:54:27 +0200

    remove login button from header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,6 +9,7 @@ import 'react-date-range/dist/theme/default.css'; // theme css file
 import { format } from 'date-fns'
 import { useNavigate } from 'react-router-dom'
 import { SearchContext } from '../../context/SearchContext'
+import { AuthContext } from '../../context/AuthContext'
 
 const Header = ({ type }) => {
     const [destination, setDestination] = useState ("");
@@ -26,6 +27,8 @@ const Header = ({ type }) => {
     })
 
     const navigate = useNavigate()
+
+    const {user} = useContext(AuthContext)
 
     const handleOption = (name, operation) => {
         setOptions((prev) => {
@@ -72,7 +75,7 @@ const Header = ({ type }) => {
                     <p className="headerDescription">
                         Get rewarded for your travels - unlock instant savings of 10%
                     </p>
-                    <button className='headerBtn'>Sign in / Register</button>
+                    {!user && (<button className='headerBtn'>Sign in / Register</button>)}
                     <div className="headerSearch">
                         <div className="headerSearchItem">
                             <FontAwesomeIcon icon={faBed} className='headerIcon' />
