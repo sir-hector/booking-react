8412e1cce29b513431669d122f10e67e05ca52ce
sir-hector <krauskarol7@gmail.com>
2022-08-01 18:52:05 +0200

    add user name to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import './navbar.css'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../../context/AuthContext'
 
 const Navbar = () => {
   const {user} = useContext(AuthContext)
-  
+
   return (
     <div className="navbar">
         <div className="navContainer">
@@ -13,10 +14,10 @@ const Navbar = () => {
               hectorbooking
               </Link>
               </span>
-            <div className="navItems">
+            {user ? user.username : (<div className="navItems">
                 <button className="navButton">Register</button>
                 <button className="navButton">Login</button>
-            </div>
+            </div>)}
         </div>
     </div>
   )
